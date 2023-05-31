c060343131a4331c6a03fbad5686e44922278040
alexsh882 <alexsh882@gmail.com>
2023-05-31 10:44:28 -0300

    Se agregó el listado de objetos al main.js

---
+++ ejercicio-2/main.js
@@ -0,0 +1,17 @@
+const listado = [
+  {
+    titulo: "Titulo 1",
+    descripcion: "Descripción de la card Nro 1.",
+    imagen_src: "https://img.freepik.com/foto-gratis/cerca-barriles-vino-metalicos-bodega_1268-15697.jpg?w=826&t=st=1685536868~exp=1685537468~hmac=d946a6702d2c30ecbea97fcbc2f5f7d51ec84a722c01df0b1de67d9d9b0a8198",
+  },
+  {
+    titulo: "Titulo 2",
+    descripcion: "Descripción de la card Nro 2.",
+    imagen_src: "https://img.freepik.com/foto-gratis/ingenieros-ambientales-trabajando-juntos_23-2149352243.jpg?w=740&t=st=1685536937~exp=1685537537~hmac=b768b3bb790ad016f01de254a0c9d924fcbf985b94098803845e21730e4ea274",
+  },
+  {
+    titulo: "Titulo 3",
+    descripcion: "Descripción de la card Nro 3.",
+    imagen_src: "https://as1.ftcdn.net/v2/jpg/04/85/33/58/1000_F_485335817_AZCIC04XDrecAIxrLZ0YBJFtgzXUT3xC.jpg",
+  },
+];
