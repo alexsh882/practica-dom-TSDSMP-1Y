39107628b8a753b838a7c9d91084917ec9e5f22e
alexsh882 <alexsh882@gmail.com>
2023-05-31 11:48:07 -0300

    Se agregaron las funcionalidades en el main.js para realizar las operaciones

---
+++ ejercicio-3/main.js
@@ -0,0 +1,26 @@
+let numero1 = document.getElementById("numero1");
+let numero2 = document.getElementById("numero2");
+let select = document.getElementById("select");
+let calcular = document.getElementById("calcular");
+let resultado = document.getElementById("resultado");
+
+const realizarCalculo = () => {
+  switch (select.value) {
+    case "suma":
+      resultado.innerText = parseInt(numero1.value) + parseInt(numero2.value);
+      break;
+    case "resta":
+      resultado.innerText = parseInt(numero1.value) - parseInt(numero2.value);
+      break;
+    case "multiplicacion":
+      resultado.innerText = parseInt(numero1.value) * parseInt(numero2.value);
+      break;
+    case "division":
+      resultado.innerText = parseInt(numero1.value) / parseInt(numero2.value);
+      break;
+
+    default:
+      resultado.innerText = "";
+      break;
+  }
+};
